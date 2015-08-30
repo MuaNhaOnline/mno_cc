f236e8aee5c5e198eeeccf1d00a0effd4f501f3a
Le Binh Chieu <lebinhchieu@gmail.com>
2015-08-30 11:03:17 +0700

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,7 +47,7 @@ class ApplicationController < ActionController::Base
       result = User.check_signin_without_encode cookies[:user_account], cookies[:user_password]
       
       if result[:status] === 0
-        User.current_user = @current_user = result[:result]
+        User.current = @current_user = result[:result]
         session[:user_id] = result[:result].id
         return
       end
