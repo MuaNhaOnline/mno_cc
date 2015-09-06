68e9bf7044005ae41d8283291effb62786bf8395
PenguinBlueChip <Funky_Hpio@yahoo.com>
2015-09-06 23:10:53 +0700

    nothing

--- app/assets/javascripts/projects/index.js
+++ app/assets/javascripts/projects/index.js
@@ -16,7 +16,36 @@ $(function() {
 	$('.wrapper-distributor').find('.list').slick({
 		centerMode: true,
 		centerPadding: '60px',
-		slidesToShow: 3		
+		slidesToShow: 3,
+		variableWidth: true
 	});
+
+	initScrollBox();
 });
+// end
+
+// start Scroll Box
+function initScrollBox() {
+	$(window).on('scroll', function() {
+		var scroll = $(window).scrollTop();
+		if (scroll >= 1600) {
+			$('.fix-table').removeClass('nofix');
+			$('.fix-table').addClass('fix');
+		}
+		else {
+			$('.fix-table').removeClass('fix');
+			$('.fix-table').addClass('nofix');
+		}
+	});
+}
+// end
+
+// start fix-table Select
+function initFixTable() {
+	var itemli = $('.fix-table').find('li');
+	$(itemli).on('click', function() {
+		$(itemli).removeClass('active');
+		$(this).addClass('active');
+	});
+}
 // end
