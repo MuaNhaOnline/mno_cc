8a45f1419751b2d2d8ed8d0ccc0d443f721efa06
Le Binh Chieu <lebinhchieu@gmail.com>
2015-09-21 12:09:53 +0700

    Change image

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,6 +2,6 @@ module ProjectsHelper
 	# params
 	# 	style_class
 	def project_avatar p, params = {}
-		"<img class=\"#{params[:style_class]}\" src=\"#{p.images.count > 0 ? '/images/' + p.images[0].id.to_s : '/assets/projects/default.png'}\" alt=\"#{p.title}\">".html_safe
+		"<img class=\"#{params[:style_class]}\" src=\"#{p.images.count > 0 ? image_path(p.images[0]) : '/assets/projects/default.png'}\" alt=\"#{p.title}\">".html_safe
 	end
 end
