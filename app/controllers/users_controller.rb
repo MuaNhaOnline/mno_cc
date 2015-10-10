e5819ac9a5219da541fba5ce66c2fb059c081833
Le Binh Chieu <lebinhchieu@gmail.com>
2015-10-10 22:07:37 +0700

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -169,7 +169,7 @@ class UsersController < ApplicationController
 
     # Return if not correct
     if result[:status] != 0
-      return redirect_to '/' if result[:status] === 6
+      return redirect_to '/' if result[:status] == 6
       return respond_to do |format|
         format.html { render 'users/signin', locals: { error: result[:result][:result] } }
         format.json { render json: result }
