dc877ec26f059110545994ba157f0063e58ca440
Le Binh Chieu <lebinhchieu@gmail.com>
2015-10-10 19:45:09 +0700

    a

--- app/assets/javascripts/shared/layout.js
+++ app/assets/javascripts/shared/layout.js
@@ -14,7 +14,7 @@ $(function () {
 */
 
 function isMobile() {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB10|IEMobile|Opera Mini|Touch/i.test(navigator.userAgent);
 }
 
 /*
