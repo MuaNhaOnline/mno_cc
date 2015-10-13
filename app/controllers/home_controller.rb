a20c2cb3543319a2bf86663ae4ecfb01f1301a16
Le Binh Chieu <lebinhchieu@gmail.com>
2015-10-13 11:06:50 +0700

    a

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,18 +17,42 @@ class HomeController < ApplicationController
 
   # View
   def online_counter
-    sessions = Session.where('updated_at > ?', 15.minutes.ago)
-    @all = Session.count
-    @users = []
-    @guess = 0
+    @all = Session.all
+    @online_users = []
+    @online_guess_count = 0
+    @day_count = 0
+    @month_count = 0
+    @year_count = 0
+    @in_day_users = []
 
-    sessions.each do |s|
-      if s.data['user_id'].present?
-        @users << User.where(id: s.data['user_id']).first
-      else
-        @guess += 1
+    @all.each do |s|
+      if s.updated_at > 15.minutes.ago
+        if s.data['user_id'].present?
+          @online_users << s.data['user_id']
+        else
+          @online_guess_count += 1
+        end
+      end
+
+      if s.updated_at.year == Date.current.year
+        @year_count += 1
+        if s.updated_at.month == Date.current.month
+          @month_count += 1
+          if s.updated_at.day == Date.current.day
+            @day_count += 1
+            if s.data['user_id'].present?
+              @in_day_users << s.data['user_id']
+            end
+          end
+        end
       end
     end
+
+    @online_users = @online_users.uniq
+    @in_day_users = @in_day_users.uniq
+
+    @online_users = User.find @online_users
+    @in_day_users = User.find @in_day_users
 
     render layout: 'layout_back'
   end
