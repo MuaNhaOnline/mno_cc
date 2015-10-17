4db583f2c81ac8cfa11b58b0e9b28039e3da2b80
Le Binh Chieu <lebinhchieu@gmail.com>
2015-10-17 15:04:27 +0700

    a

---
+++ app/assets/javascripts/projects/create_details.js
@@ -0,0 +1,17 @@
+$(function () {
+	initOpenButton();
+
+	/*
+		Focus buttons
+	*/
+
+		function initOpenButton() {
+			$('#a').on('click', function () {
+				$('.create-details-container').addClass('open');
+			});
+		}
+
+	/*
+		/ Focus buttons
+	*/
+})
