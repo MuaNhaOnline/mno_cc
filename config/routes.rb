1ce9c679448874083c2bfed69b6ef7080cffcb27
PenguinBlueChip <Funky_Hpio@yahoo.com>
2015-11-04 09:29:56 +0700

    nothing

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
     get 'home/index'
     get 'home/back'
     get 'home/blog'
+    get 'home/detail'
     post 'set_width/:width_type' => 'home#set_width'
     post 'nothing' => 'home#nothing'
     post 'end_session' => 'home#end_session'
