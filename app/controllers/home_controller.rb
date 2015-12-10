a3c1d2c9c7126e61f6d7d8630945f053ed54fd14
Le Binh Chieu <lebinhchieu@gmail.com>
2015-12-10 11:05:41 +0700

    a

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -106,50 +106,54 @@ class HomeController < ApplicationController
 		# Handle
 		def track_session
 			# If first time
+			# unless session[:is_not_first]
+			# 	session[:is_not_first] = true
+
+			# 	# If was not give info
+			# 	unless cookies[:was_give_info]
+			# 		# If first time in browser (use cookie to detect)
+			# 		# => Set current_session cookie data is begin session (befor save)
+			# 		# Else
+			# 		# => Set current begin_session_id to current_session (after save)
+			# 		s = Session.new
+			# 		if cookies[:begin_session_id].present?
+			# 			s.begin_session_id = cookies[:begin_session_id]
+			# 		end
+
+			# 		if request.referrer.present?
+			# 			s.referrer_host = URI(request.referrer).host.gsub(/\bwww./, '')
+			# 			s.referrer_source = request.referrer
+			# 			if s.referrer_host.include? 'facebook.com'
+			# 				s.referrer_host_name = 'Facebook'
+			# 			elsif s.referrer_host.include? 'muanhaonline.vn'
+			# 				s.referrer_host_name = 'MuanhaOnline'
+			# 			elsif s.referrer_host.include? 'google.com'
+			# 				s.referrer_host_name = 'Google'
+			# 			else
+			# 				s.referrer_host_name = s.referrer_host
+			# 			end
+			# 		else
+			# 			s.referrer_host_name = 'Direct'
+			# 		end
+
+			# 		s.utm_campaign = params[:utm_campaign] if params[:utm_campaign].present?
+			# 		s.utm_source = params[:utm_source] if params[:utm_source].present?
+			# 		s.utm_medium = params[:utm_medium] if params[:utm_medium].present?
+			# 		s.utm_term = params[:utm_term] if params[:utm_term].present?
+			# 		s.utm_content = params[:utm_content] if params[:utm_content].present?
+
+			# 		s.save
+
+			# 		if cookies[:begin_session_id].blank?
+			# 			cookies[:begin_session_id] = s.id
+			# 		end
+
+			# 		session[:current_session_id] = s.id
+			# 	end
+			# end
+
 			unless session[:is_not_first]
-				session[:is_not_first] = true
-
-				# If was not give info
-				unless cookies[:was_give_info]
-					# If first time in browser (use cookie to detect)
-					# => Set current_session cookie data is begin session (befor save)
-					# Else
-					# => Set current begin_session_id to current_session (after save)
-					s = Session.new
-					if cookies[:begin_session_id].present?
-						s.begin_session_id = cookies[:begin_session_id]
-					end
-
-					if request.referrer.present?
-						s.referrer_host = URI(request.referrer).host.gsub(/\bwww./, '')
-						s.referrer_source = request.referrer
-						if s.referrer_host.include? 'facebook.com'
-							s.referrer_host_name = 'Facebook'
-						elsif s.referrer_host.include? 'muanhaonline.vn'
-							s.referrer_host_name = 'MuanhaOnline'
-						elsif s.referrer_host.include? 'google.com'
-							s.referrer_host_name = 'Google'
-						else
-							s.referrer_host_name = s.referrer_host
-						end
-					else
-						s.referrer_host_name = 'Direct'
-					end
-
-					s.utm_campaign = params[:utm_campaign] if params[:utm_campaign].present?
-					s.utm_source = params[:utm_source] if params[:utm_source].present?
-					s.utm_medium = params[:utm_medium] if params[:utm_medium].present?
-					s.utm_term = params[:utm_term] if params[:utm_term].present?
-					s.utm_content = params[:utm_content] if params[:utm_content].present?
-
-					s.save
-
-					if cookies[:begin_session_id].blank?
-						cookies[:begin_session_id] = s.id
-					end
-
-					session[:current_session_id] = s.id
-				end
+				Session.find(sessions[current_session_id]).update(user_info_type: nil) if session[:current_session_id].present?
 			end
 
 			render nothing: true
